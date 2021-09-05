b7f2b4f402657e2d57fc6f22a37a88ae22f4b645
Alisher <49154018+alisherrahimov@users.noreply.github.com>
2021-09-05 19:11:09 +0500

    Update Main.java

--- android/app/src/main/java/com/reduxlearn/Main.java
+++ android/app/src/main/java/com/reduxlearn/Main.java
@@ -68,6 +68,46 @@ public class Main extends ReactContextBaseJavaModule {
             promise.reject("Error", error);
         }
     }
+    @SuppressLint("Range")
+    @ReactMethod
+    public void getContact(Promise promise) {
+        try {
+            WritableArray contact = new WritableNativeArray();
+            Cursor cr = reactContext.getContentResolver().query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+            if ((cr != null ? cr.getCount() : 0) > 0) {
+                while (cr != null && cr.moveToNext()) {
+                    WritableMap info = new WritableNativeMap();
+                    String id = cr.getString(cr.getColumnIndex(ContactsContract.Contacts._ID));
+                    String name = cr.getString(cr.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+
+                    if (cr.getInt(cr.getColumnIndex(
+                            ContactsContract.Contacts.HAS_PHONE_NUMBER)) > 0) {
+                        Cursor pCur = reactContext.getContentResolver().query(
+                                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                                null,
+                                ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                                new String[]{id}, null);
+                        while (pCur.moveToNext()) {
+                            String phoneNo = pCur.getString(pCur.getColumnIndex(
+                                    ContactsContract.CommonDataKinds.Phone.NUMBER));
+                            info.putString("id", id);
+                            info.putString("name", name);
+                            info.putString("number", phoneNo);
+
+                        }
+                        pCur.close();
+                    }
+                    contact.pushMap(info);
+                }
+            }
+            if (cr != null) {
+                cr.close();
+            }
+            promise.resolve(contact);
+        } catch (Exception error) {
+            promise.reject("Error", error);
+        }
+    }
 
 
 }
